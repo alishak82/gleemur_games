37203cd8cbbeb825965b64c7299354a7903e45c0
alishak82 <alisha82@gmail.com>
2014-11-10 13:52:22 -0500

    more changes

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   get '/new' do
+    api_response = HTTParty.get("http://www.colourlovers.com/api/palettes/random")
+    @title = api_response['palettes']['palette']['title']
+    @hexs = api_response['palettes']['palette']['colors']['hex']
     erb :'users/new'
   end
 
